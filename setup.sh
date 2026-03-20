0f99dad75dcbc962bd6f5f983c25de1d219571f4
melinui <melina.plmk@gmail.com>
2026-03-20 19:42:29 +0200

    Mod setup script and add command for making the game script executable

--- setup.sh
+++ setup.sh
@@ -10,4 +10,7 @@ else
     curl -s 'https://gist.githubusercontent.com/daemondevin/df09befaf533c380743bc2c378863f0c/raw/5-letter-words.txt' > words.txt
 fi
 
+# just in case make game script executable
+chmod a+x game.sh
+
 echo "Setup is done. You can start playing!"
